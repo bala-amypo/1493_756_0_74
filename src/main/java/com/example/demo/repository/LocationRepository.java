31055ce8c957f8c36fd633637ce54e9335b289a0
bala-amypo <bala@amypo.in>
2025-12-17 08:56:45 +0000

    User commit

--- src/main/java/com/example/demo/repository/LocationRepository.java
+++ src/main/java/com/example/demo/repository/LocationRepository.java
@@ -6,12 +6,3 @@ import org.springframework.stereotype.Repository;
 public interface LocationRepository extends JpaRepository<LocationEntity, Long> {
 }
 
-package com.example.demo.repository;
-
-import com.example.demo.entity.LocationEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LocationRepository extends JpaRepository<LocationEntity, Long> {
-}
--- src/main/java/com/example/demo/service/LocationServiceImp.java
+++ src/main/java/com/example/demo/service/LocationServiceImp.java
@@ -16,3 +16,31 @@ public class LocationService implements LocationService {
         return Locationrepo.findAll();
     }
 }
+
+
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.entity.LocationEntity;
+import com.example.demo.repository.LocationRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LocationServiceImp implements LocationService {
+
+    @Autowired
+    private LocationRepository locationRepo;
+
+    @Override
+    public LocationEntity createlocation(LocationEntity le) {
+        return locationRepo.save(le);
+    }
+
+    @Override
+    public List<LocationEntity> getAllLocations() {
+        return locationRepo.findAll();
+    }
+}
