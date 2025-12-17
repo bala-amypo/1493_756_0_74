c8b5c9b6769e318d4d6133028b7f99b66cbacbd7
bala-amypo <bala@amypo.in>
2025-12-17 09:08:56 +0000

    User commit

--- src/main/java/com/example/demo/controller/LocationController.java
+++ src/main/java/com/example/demo/controller/LocationController.java
@@ -1,25 +1,26 @@
 package com.example.demo.controller;
 
 import java.util.List;
-import java.lang.reflect.Accessflag.LocationEntity;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.LocationEntity;
 import com.example.demo.service.LocationService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class LocationController {
+
     @Autowired
-    LocationService locationservice;
-    @PostMapping("/addstudent")
-    public LocationEntity createlocation(@RequestBody LocationEntity le){
-        return locationservice.createlocation(le);
+    private LocationService locationService;
+
+    @PostMapping("/addlocation")
+    public LocationEntity createlocation(@RequestBody LocationEntity le) {
+        return locationService.createlocation(le);
     }
+
     @GetMapping("/showlocations")
-    public List<LocationEntity> showlocations(){
-        return locationservice.getAllLocations();
+    public List<LocationEntity> showlocations() {
+        return locationService.getAllLocations();
     }
 }
